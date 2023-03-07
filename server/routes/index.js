4abf1d0bdbe5b0e4cd4ffd9b288d8d7a103ded03
Luke Page <kelly.luke2705@gmail.com>
2023-03-07 09:31:26 +0000

    Code Added

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,31 @@
-const express = require("express");
-const router = express.Router();
+const router = require('express').Router();
+const {Page, User, Tag} = require('../models');
 
-// different model routers
-router.use('/wiki', require('./wiki'));
-router.use('/users', require('./users'));
+router.get('/wiki', async (req, res, next) => {
+  try {
+    const pages = await Page.findAll();
+    res.send(pages);
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router;
+router.get('/wiki/:slug', async (req, res, next) => {
+  try {
+    const page = await Page.findOne({
+      where: {slug: req.params.slug},
+      include: [{model: User, as: 'author'}]
+    });
+    res.send(page);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/wiki', async (req, res, next) => {
+  try {
+    const {title, content, name, email, tags} = req.body;
+    const [user, wasCreated] = await User.findOrCreate({
+      where: {name, email}
+    });
+    const page = await Page.create({title, content, author
